664a388b3b9a6b1f71ab27466a12deff5f642971
refat5 <mdrefat382139@gmail.com>
2022-02-25 06:04:11 +0600

    single vue component done

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,18 @@
 <template>
   <div id="app">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+  <navbar></navbar>
 
   <div class="container">
    <div class="row">
       <div class="col-sm-9  d-flex justify-content-between">
-              <div class="card" style="width: 12rem;">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">This is A Pizza</h5>
-                  <p class="card-text">This is A pizza do You like this?</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-
-              <div class="card" style="width: 12rem;">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">This is A Pizza</h5>
-                  <p class="card-text">This is A pizza do You like this?</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-              <div class="card" style="width: 12rem;">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">This is A Pizza</h5>
-                  <p class="card-text">This is A pizza do You like this?</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-              <div class="card" style="width: 12rem;">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">This is A Pizza</h5>
-                  <p class="card-text">This is A pizza do You like this?</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+             <product></product>
 
             
       </div>
 
       <div class="col-sm-3">
-      <ul class="list-group">
-        <li class="list-group-item">Item - Price</li>
-        <li class="list-group-item">Item - Price</li>
-        <li class="list-group-item">Item - Price</li>
-        <li class="list-group-item">Item - Price</li>
-
-
-      </ul>
-
+ 
+          <cart></cart>
       </div>
    </div>
   </div>
@@ -74,8 +21,18 @@
 
 <script>
 
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Product from './components/Product';
 
-export default {
+export default{
+    components:{
+        Navbar,
+        Cart,
+        Product
+
+    }
+
 }
 </script>
 
---
+++ src/components/Product.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+            <div class="card" style="width: 12rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">This is A Pizza</h5>
+                  <p class="card-text">This is A pizza do You like this?</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+              <div class="card" style="width: 12rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">This is A Pizza</h5>
+                  <p class="card-text">This is A pizza do You like this?</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+              <div class="card" style="width: 12rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">This is A Pizza</h5>
+                  <p class="card-text">This is A pizza do You like this?</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+              <div class="card" style="width: 12rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCstGnwN8sGGaLNG9i02mRyKsTs0F_Eb_GaQ&usqp=CAU" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">This is A Pizza</h5>
+                  <p class="card-text">This is A pizza do You like this?</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+    </div>
+</template>
+
+<script>
+export default{}
+</script>
+
+<style></style>
